59f40d529202954bbc9997fb539a7edf97dab0cf
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-05-28 17:21:06 -0500

    creando nueva pagina avatar

---
+++ src/app/pages/avatar/avatar.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-avatar",
+  templateUrl: "./avatar.page.html",
+  styleUrls: ["./avatar.page.scss"],
+})
+export class AvatarPage implements OnInit {
+  constructor() {}
+  //#region variables
+  //#endregion variables
+  ngOnInit() {}
+
+  //#region methods
+
+  //#endregion methods
+}
+
+//#region Interfaces
+
+//#endregion Interfaces
