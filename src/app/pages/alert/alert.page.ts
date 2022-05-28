92b36e197298a71acff88e787fa066eedab30c11
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-05-28 16:24:36 -0500

    show Alert

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-alert',
-  templateUrl: './alert.page.html',
-  styleUrls: ['./alert.page.scss'],
+  selector: "app-alert",
+  templateUrl: "./alert.page.html",
+  styleUrls: ["./alert.page.scss"],
 })
 export class AlertPage implements OnInit {
+  constructor(public alertController: AlertController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  //#region methods
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
+  //#endregion methods
 }
+
+//#region Interfaces
+interface AlertButton {
+  text: string;
+  role?: "cancel" | "destructive" | string;
+  cssClass?: string | string[];
+  handler?: (value: any) => boolean | void | { [key: string]: any };
+}
+
+interface AlertInput {
+  type?: TextFieldTypes | "checkbox" | "radio" | "textarea";
+  name?: string;
+  placeholder?: string;
+  value?: any;
+  label?: string;
+  checked?: boolean;
+  disabled?: boolean;
+  id?: string;
+  handler?: (input: AlertInput) => void;
+  min?: string | number;
+  max?: string | number;
+  cssClass?: string | string[];
+  attributes?: { [key: string]: any };
+  tabindex?: number;
+}
+
+interface AlertOptions {
+  header?: string;
+  subHeader?: string;
+  message?: string | IonicSafeString;
+  cssClass?: string | string[];
+  inputs?: AlertInput[];
+  buttons?: (AlertButton | string)[];
+  backdropDismiss?: boolean;
+  translucent?: boolean;
+  animated?: boolean;
+  htmlAttributes?: { [key: string]: any };
+
+  mode?: Mode;
+  keyboardClose?: boolean;
+  id?: string;
+
+  enterAnimation?: AnimationBuilder;
+  leaveAnimation?: AnimationBuilder;
+}
+//#endregion Interfaces
