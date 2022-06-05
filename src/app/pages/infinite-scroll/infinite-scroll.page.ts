136be68851ad72a30a6d73af398e4ff3b18d1357
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-06-04 19:42:31 -0500

    infinite scroll  test

--- src/app/pages/infinite-scroll/infinite-scroll.page.ts
+++ src/app/pages/infinite-scroll/infinite-scroll.page.ts
@@ -7,13 +7,28 @@ import { Component, OnInit } from "@angular/core";
 })
 export class InfiniteScrollPage implements OnInit {
   //#region variables
-  items: any[] = Array(12).fill('Hola')
+  items: any[] = Array(12).fill("Hola");
   //#endregion variables
   constructor() {}
   ngOnInit() {}
 
   //#region methods
+  loadData(event) {
+    setTimeout(() => {
+      console.log("Done");
+      event.target.complete();
 
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (data.length === 1000) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  }
   //#endregion methods
 }
 
