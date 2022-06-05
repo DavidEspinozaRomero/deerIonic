07686af0f9d442644d5d5384770a36d1a826e227
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-06-05 14:46:21 -0500

    snipet

--- src/app/pages/input/input.page.ts
+++ src/app/pages/input/input.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-input",
@@ -7,13 +8,21 @@ import { Component, OnInit } from "@angular/core";
 })
 export class InputPage implements OnInit {
   //#region variables
-  items: any[] = Array(12).fill('Hola')
+  items: any[] = Array(12).fill("Hola");
+
+  loginForm: FormGroup = this._fb.group({
+    email: ["", [Validators.email, Validators.required]],
+    password: ["", [Validators.minLength(6), Validators.required]],
+  });
   //#endregion variables
-  constructor() {}
+  constructor(private _fb: FormBuilder) {}
   ngOnInit() {}
 
   //#region methods
-
+  onSubmit(event:any){
+    console.log(event);
+    
+  }
   //#endregion methods
 }
 
