eb2c20e8a30b2fc2d17239b5b45fc5867c05263e
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-06-05 16:43:42 -0500

    getUsers() {

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -16,7 +16,7 @@ export class ListPage implements OnInit {
 
   //#region Apis
   getUsers() {
-    this.users = this._dataservice.getUsers().subscribe(console.log);
+    this.users = this._dataservice.getUsers();
   }
 
   //#endregion Apis
