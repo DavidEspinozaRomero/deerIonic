c6c27da3186a39124f3565cb173fb14e749aa535
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-06-05 18:21:22 -0500

    (ionItemReorder)="doReorder($event)">

--- src/app/pages/list-reorder/list-reorder.page.ts
+++ src/app/pages/list-reorder/list-reorder.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonReorderGroup } from '@ionic/angular';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 @Component({
   selector: "app-list-reorder",
@@ -7,8 +9,8 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ListReorderPage implements OnInit {
   //#region variables
+  @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup;
   items: any[] = Array(20)
-  // @ViewChild('list') list: IonList
   //#endregion variables
 
   constructor() {}
@@ -20,6 +22,20 @@ export class ListReorderPage implements OnInit {
   //#endregion Apis
 
   //#region methods
+  doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
+
+  // toggleReorderGroup() {
+  //   this.reorderGroup.disabled = !this.reorderGroup.disabled;
+  // }
   //#endregion methods
 }
 
