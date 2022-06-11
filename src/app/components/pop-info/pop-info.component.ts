171abdd757c90a61f24bb4f24e1a67a384710da4
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-06-11 18:44:32 -0500

    ngOnInit

--- src/app/components/pop-info/pop-info.component.ts
+++ src/app/components/pop-info/pop-info.component.ts
@@ -10,4 +10,9 @@ export class PopInfoComponent implements OnInit {
 
   constructor() {}
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
 }
