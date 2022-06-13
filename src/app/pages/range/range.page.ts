0316002f0ea48a9cafd704db0a13ca367552a02d
David Emanuel Espinoza Romero <deerhou@gmail.com>
2022-06-12 19:47:06 -0500

    range err

--- src/app/pages/range/range.page.ts
+++ src/app/pages/range/range.page.ts
@@ -23,10 +23,7 @@ export class RangePage implements OnInit {
     return `${value}%`;
   }
 
-  rangeChange(event: any) {
-    // console.log(event.detail.value);
-    this.porcent = (event.detail.value / 100);
-  }
+
   //#endregion methods
 }
 
